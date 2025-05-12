20b4c0d4eaf25c14c5bb1338033f7abdd988fb04
Camila Videla <camilavidela@Camilas-MacBook-Air.local>
2025-05-12 08:02:46 -0400

    Elimina vista de resumen que ya no se usa

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "33ff9fcc3752eb82"; }
+/******/ 		__webpack_require__.h = function() { return "b702305777dfbdba"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
