3007ead611035289832677a43fca6968d4f3d5d2
Camila Videla <camilavidela@Camilas-MacBook-Air.local>
2025-07-07 14:55:06 -0400

    🚀 Primer commit del proyecto checklist-asmcontrol

--- firebase/firebaseConfig.ts
+++ firebase/firebaseConfig.ts
@@ -4,12 +4,12 @@ import { getStorage } from 'firebase/storage';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'TU_API_KEY',
-  authDomain: 'checklist-asmcontrol.firebaseapp.com',
-  projectId: 'checklist-asmcontrol',
-  storageBucket: 'checklist-asmcontrol.firebasestorage.app',
-  messagingSenderId: 'TU_MESSAGING_SENDER_ID',
-  appId: 'TU_APP_ID'
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
@@ -17,6 +17,7 @@ const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
 export const auth = getAuth(app);
+
 
 
 
