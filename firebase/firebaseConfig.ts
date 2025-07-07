cf045483eb459b2e04013e7c164d58a0632c1301
Camila Videla <camilavidela@Camilas-MacBook-Air.local>
2025-07-07 15:01:36 -0400

    🚀 Primer commit del proyecto checklist-asmcontrol

--- firebase/firebaseConfig.ts
+++ firebase/firebaseConfig.ts
@@ -1,3 +1,6 @@
+// firebase/firebaseConfig.ts
+console.log('API KEY:', process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
+
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
@@ -17,6 +20,7 @@ const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
 export const auth = getAuth(app);
+
 
 
 
